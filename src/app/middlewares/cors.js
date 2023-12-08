c2bbba1d46acba75684835de1c16977266147ccf
Matheus F <matheus_falmeida@hotmail.com>
2023-12-07 22:41:00 -0300

    Adjusting CORS

--- src/app/middlewares/cors.js
+++ src/app/middlewares/cors.js
@@ -1,6 +1,5 @@
-
 module.exports = (request, response, next) => {
-  response.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000' || process.env.BASE_URL)
+  response.setHeader('Access-Control-Allow-Origin', '*')
   response.setHeader('Access-Control-Allow-Methods', '*')
   response.setHeader('Access-Control-Allow-Headers', '*')
   next()
